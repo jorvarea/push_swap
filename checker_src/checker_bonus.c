5f076af1e061efa4415593d872a5bdc802c1f6b8
jorvarea <jorvarea@users.noreply.github.com>
2024-01-18 21:52:56 +0100

    refactoring checker

--- checker_src/checker_bonus.c
+++ checker_src/checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 13:36:06 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/18 21:43:12 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/18 21:50:28 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,22 @@
 #include "libft.h"
 #include "push_swap_utils.h"
 
+void handle_output(bool a_is_null, bool b_is_null, bool instruction_error, bool is_sorted)
+{
+	if (!a_is_null && !instruction_error && b_is_null && is_sorted)
+		ft_printf("OK\n");
+	else if (!a_is_null && !instruction_error)
+		ft_printf("KO\n");
+	else
+		ft_putstr_fd("Error\n", STDERR_FILENO);
+}
+
 int	main(int argc, char **argv)
 {
 	t_list	*a;
 	t_list	*b;
 	char	*line;
 	bool	instruction_error;
-	bool 	is_sorted;
 
 	initialize_stacks(argc, argv, &a, &b);
 	if (a != NULL)
@@ -38,13 +47,7 @@ int	main(int argc, char **argv)
 			print_stacks(a, b);
 		}
 	}
-	is_sorted = is_stack_sorted(a);
-	if (a != NULL && !instruction_error && b == NULL && is_sorted)
-		ft_printf("OK\n");
-	else if (a != NULL && !instruction_error)
-		ft_printf("KO\n");
-	else
-		ft_putstr_fd("Error\n", STDERR_FILENO);
+	handle_output(a == NULL, b == NULL, instruction_error, is_stack_sorted(a));
 	clear_list(&a);
 	clear_list(&b);
 	return (0);
