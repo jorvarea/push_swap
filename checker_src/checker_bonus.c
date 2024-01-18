fb31f9af2b21362f19f0e2e7726adb334f880a99
jorvarea <jorvarea@users.noreply.github.com>
2024-01-18 16:55:31 +0100

    stack operations work

--- checker_src/checker_bonus.c
+++ checker_src/checker_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 13:36:06 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/18 16:34:23 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/18 16:42:59 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,27 @@
 void process_instruction(char *line, t_list **a, t_list **b, bool *error)
 {
 	*error = false;
-	if (ft_strcmp(line, "sa") == 0)
+	if (ft_strcmp(line, "sa\n") == 0)
 		swap_top_stack(a);
-	else if (ft_strcmp(line, "sb") == 0)
+	else if (ft_strcmp(line, "sb\n") == 0)
 		swap_top_stack(b);
-	else if (ft_strcmp(line, "ss") == 0)
+	else if (ft_strcmp(line, "ss\n") == 0)
 		swap_top_stack_both(a, b);
-	else if (ft_strcmp(line, "pa") == 0)
+	else if (ft_strcmp(line, "pa\n") == 0)
 		push_top_stack(a, b);
-	else if (ft_strcmp(line, "pb") == 0)
+	else if (ft_strcmp(line, "pb\n") == 0)
 		push_top_stack(b, a);
-	else if (ft_strcmp(line, "ra") == 0)
+	else if (ft_strcmp(line, "ra\n") == 0)
 		rotate_stack_up(a);
-	else if (ft_strcmp(line, "rb") == 0)
+	else if (ft_strcmp(line, "rb\n") == 0)
 		rotate_stack_up(b);
-	else if (ft_strcmp(line, "rr") == 0)
+	else if (ft_strcmp(line, "rr\n") == 0)
 		rotate_stack_up_both(a, b);
-	else if (ft_strcmp(line, "rra") == 0)
+	else if (ft_strcmp(line, "rra\n") == 0)
 		rotate_stack_down(a);
-	else if (ft_strcmp(line, "rrb") == 0)
+	else if (ft_strcmp(line, "rrb\n") == 0)
 		rotate_stack_down(b);
-	else if (ft_strcmp(line, "rrr") == 0)
+	else if (ft_strcmp(line, "rrr\n") == 0)
 		rotate_stack_down_both(a, b);
 	else
 		*error = true;
