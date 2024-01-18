70fc2ab2f77288ae36f2ac4e5a52f3d7b5dc2429
jorvarea <jorvarea@users.noreply.github.com>
2024-01-18 16:08:25 +0100

    changed strncmp to strcmp

--- lib/libft/ft_strcmp.c
+++ lib/libft/ft_strcmp.c
@@ -6,24 +6,18 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:10:42 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/18 15:11:03 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/18 16:07:16 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strcmp(char *s1, char *s2)
 {
-	unsigned int	i;
+	int	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n
-		&& (unsigned char)s1[i] == (unsigned char)s2[i])
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
-	if (n == 0)
-		return (0);
-	else if (i == n)
-		return ((unsigned char)s1[i - 1] - (unsigned char)s2[i - 1]);
-	else
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (s1[i] - s2[i]);
 }
