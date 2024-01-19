0a544db8ff1ba9ca738ab3609d974ca8f23d3649
jorvarea <jorvarea@users.noreply.github.com>
2024-01-19 15:32:42 +0100

    changed back to selection sort

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 23:19:33 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/19 14:18:13 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/19 14:39:52 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv)
 
 	initialize_stacks(argc, argv, &a, &b);
 	if (a != NULL)
-		stack_bubble_sort(&a);
+		stack_selection_sort(&a, &b, argc - 1);
 	else
 		ft_putstr_fd("Error\n", STDERR_FILENO);
 	clear_list(&a);
