cfe8864330548097138314a7ffc870658721f67d
jorvarea <jorvarea@c2r2s1.42malaga.com>
2024-01-29 22:23:23 +0100

    just need to finish execute_optimal_move

--- src/lis_based_sorting.c
+++ src/lis_based_sorting.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 14:23:20 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/29 21:14:23 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/29 21:49:31 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,78 +31,6 @@ static void	push_unsorted(t_list **a, t_list **b, int *lis, int stack_len)
 		}
 		i++;
 	}
-}
-
-static void	calculate_moves_b(t_list *b, int **moves_b)
-{
-	int	size;
-	int	i;
-
-	size = list_size(b);
-	*moves_b = malloc(size * sizeof(int));
-	if (*moves_b)
-	{
-		i = 0;
-		while (i < size)
-		{
-			if (i <= size / 2)
-				(*moves_b)[i] = i;
-			else
-				(*moves_b)[i] = -(size - i);
-			i++;
-		}
-	}
-}
-
-static void	calculate_moves_a(t_list *a, t_list *b, int **moves_a)
-{
-	t_list	*current_a;
-	t_list	*current_b;
-	int		size_a;
-	int		i;
-	int		j;
-
-	*moves_a = malloc(list_size(b) * sizeof(int));
-    if (*moves_a)
-    {
-        size_a = list_size(a);
-        i = 0;
-        current_b = b;
-        while (current_b)
-        {
-            j = 0;
-            current_a = a;
-            while (current_a && current_a->number < current_b->number)
-            {
-                current_a = current_a->next;
-                j++;
-            }
-            if (j <= size_a / 2)
-                (*moves_a)[i] = j;
-            else
-                (*moves_a)[i] = -(size_a - j);
-            current_b = current_b->next;
-            i++;
-        }
-    }
-}
-
-static void	insert_unsorted(t_list **a, t_list **b)
-{
-	int	*moves_a;
-	int	*moves_b;
-
-	calculate_moves_b(*b, &moves_b);
-	calculate_moves_a(*a, *b, &moves_a);
-    print_stacks(*a, *b);
-    for (int i = 0; i < list_size(*b); i++)
-        ft_printf("%d, ", moves_a[i]);
-    ft_printf("\n");
-    for (int i = 0; i < list_size(*b); i++)
-        ft_printf("%d, ", moves_b[i]);
-    ft_printf("\n");
-    free(moves_a);
-    free(moves_b);
 }
 
 void	lis_based_sorting(t_list **a, t_list **b, int stack_len)
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 23:19:58 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/29 14:38:23 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/29 22:16:13 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,14 @@ typedef struct s_lis_arrays
 	int	size;
 }		t_lis_arrays;
 
+typedef struct s_moves
+{
+	int *a;
+	int *b;
+}		t_moves;
+
 void	stack_selection_sort(t_list **a, t_list **b, int stack_len);
+void	insert_unsorted(t_list **a, t_list **b);
 void    lis_based_sorting(t_list **a, t_list **b, int stack_len);
 int		*longest_increasing_subsequence(int *numbers, int size);
 
