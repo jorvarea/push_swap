12ca6c6c8c886d1f03031d04e0319f856b00ee18
jorvarea <jorvarea@users.noreply.github.com>
2024-01-31 23:09:12 +0100

    moving functions around

--- random_numbers.py
+++ random_numbers.py
@@ -2,20 +2,12 @@ import random
 
 def partial_shuffle(lst, shuffle_fraction=0.5):
     """Shuffles part of the list based on the specified shuffle fraction."""
-    # Calculate the number of elements to shuffle
     num_elements_to_shuffle = int(len(lst) * shuffle_fraction)
-    
-    # Extract the elements to shuffle
     elements_to_shuffle = lst[:num_elements_to_shuffle]
-    
-    # Shuffle the extracted elements
     random.shuffle(elements_to_shuffle)
-    
-    # Replace the shuffled elements back into the list
     lst[:num_elements_to_shuffle] = elements_to_shuffle
     return lst
 
-# Example usage:
 numbers = list(range(500))
 shuffled_numbers = partial_shuffle(numbers, shuffle_fraction=0.3)
 numbers_str = ' '.join(str(num) for num in shuffled_numbers)
--- src/push_unsorted.c
+++ src/push_unsorted.c
@@ -6,11 +6,28 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 22:59:04 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/01/31 22:59:27 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/01/31 23:07:46 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	calculate_moves(t_list *b, int *moves_b)
+{
+	int	size;
+	int	i;
+
+	size = list_size(b);
+	i = 0;
+	while (i < size)
+	{
+		if (i <= size / 2)
+			moves_b[i] = i;
+		else
+			moves_b[i] = -(size - i);
+		i++;
+	}
+}
 
 void	push_unsorted(t_list **a, t_list **b, int *lis, int lis_size)
 {
