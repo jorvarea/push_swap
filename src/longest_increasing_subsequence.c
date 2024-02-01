5f9222ca0cd38504d4b95f85fc5644e63db680fe
jorvarea <jorvarea@users.noreply.github.com>
2024-02-01 02:24:02 +0100

    refactoring

--- src/longest_increasing_subsequence.c
+++ src/longest_increasing_subsequence.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 18:32:04 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/02/01 02:14:31 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/02/01 02:23:14 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,69 +20,6 @@ static void	free_arrays(t_lis_arrays *arrays)
 		free(arrays->lis_ending_at);
 	if (arrays->previous)
 		free(arrays->previous);
-}
-
-static void	retrieve_subsequence(t_lis_arrays *arrays, int lis_ending_index)
-{
-	int	current_i;
-	int	i;
-
-	arrays->lis[0] = arrays->numbers[lis_ending_index];
-	current_i = lis_ending_index;
-	i = 1;
-	while (arrays->previous[current_i] != -1)
-	{
-		arrays->lis[i] = arrays->numbers[arrays->previous[current_i]];
-		current_i = arrays->previous[current_i];
-		i++;
-	}
-}
-
-static int	find_lis_ending_at_index(t_lis_arrays *arrays, int index)
-{
-	int	lis_ending_at_index;
-	int	i;
-
-	lis_ending_at_index = 1;
-	i = index;
-	while (i >= 0)
-	{
-		if (arrays->numbers[i] < arrays->numbers[index]
-			&& arrays->lis_ending_at[i] + 1 > lis_ending_at_index)
-		{
-			lis_ending_at_index = arrays->lis_ending_at[i] + 1;
-			arrays->previous[index] = i;
-		}
-		i--;
-	}
-	return (lis_ending_at_index);
-}
-
-static void	find_lis(t_lis_arrays *arrays, int *lis_size)
-{
-	int	i;
-	int	lis_ending_index;
-
-	i = 0;
-	while (i < arrays->size)
-	{
-		arrays->previous[i] = -1;
-		i++;
-	}
-	*lis_size = 1;
-	lis_ending_index = 0;
-	i = 0;
-	while (i < arrays->size)
-	{
-		arrays->lis_ending_at[i] = find_lis_ending_at_index(arrays, i);
-		if (arrays->lis_ending_at[i] > *lis_size)
-		{
-			*lis_size = arrays->lis_ending_at[i];
-			lis_ending_index = i;
-		}
-		i++;
-	}
-	retrieve_subsequence(arrays, lis_ending_index);
 }
 
 int	*longest_increasing_subsequence(int *numbers, int stack_size, int *lis_size)
---
+++ src/longest_increasing_subsequence_utils.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   longest_increasing_subsequence_utils.c             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 02:19:45 by jorvarea          #+#    #+#             */
+/*   Updated: 2024/02/01 02:23:10 by jorvarea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	retrieve_subsequence(t_lis_arrays *arrays, int lis_ending_index)
+{
+	int	current_i;
+	int	i;
+
+	arrays->lis[0] = arrays->numbers[lis_ending_index];
+	current_i = lis_ending_index;
+	i = 1;
+	while (arrays->previous[current_i] != -1)
+	{
+		arrays->lis[i] = arrays->numbers[arrays->previous[current_i]];
+		current_i = arrays->previous[current_i];
+		i++;
+	}
+}
+
+static int	find_lis_ending_at_index(t_lis_arrays *arrays, int index)
+{
+	int	lis_ending_at_index;
+	int	i;
+
+	lis_ending_at_index = 1;
+	i = index;
+	while (i >= 0)
+	{
+		if (arrays->numbers[i] < arrays->numbers[index]
+			&& arrays->lis_ending_at[i] + 1 > lis_ending_at_index)
+		{
+			lis_ending_at_index = arrays->lis_ending_at[i] + 1;
+			arrays->previous[index] = i;
+		}
+		i--;
+	}
+	return (lis_ending_at_index);
+}
+
+static void initialize_previous(int *previous, int size)
+{
+    int i;
+
+    i = 0;
+    while (i < size)
+    {
+        previous[i] = -1;
+        i++;
+    }
+}
+
+void	find_lis(t_lis_arrays *arrays, int *lis_size)
+{
+	int	i;
+	int	lis_ending_index;
+
+    initialize_previous(arrays->previous, arrays->size);
+	*lis_size = 1;
+	lis_ending_index = 0;
+	i = 0;
+	while (i < arrays->size)
+	{
+		arrays->lis_ending_at[i] = find_lis_ending_at_index(arrays, i);
+		if (arrays->lis_ending_at[i] > *lis_size)
+		{
+			*lis_size = arrays->lis_ending_at[i];
+			lis_ending_index = i;
+		}
+		i++;
+	}
+	retrieve_subsequence(arrays, lis_ending_index);
+}
