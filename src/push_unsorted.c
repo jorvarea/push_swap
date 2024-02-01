4f541a24498795449163e1314148cf26f34843d2
jorvarea <jorvarea@users.noreply.github.com>
2024-02-01 01:04:40 +0100

    added logic to rotate properly

--- src/push_unsorted.c
+++ src/push_unsorted.c
@@ -6,93 +6,96 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 22:59:04 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/02/01 00:09:18 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/02/01 01:01:11 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void ft_swap(int *a, int *b)
+static int	calculate_median(t_list *a, int unsorted_size, int *lis,
+		int lis_size)
 {
-    int tmp;
+	t_list	*current;
+	int		*unsorted;
+	int		median;
+	int		i;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	unsorted = malloc(unsorted_size * sizeof(int));
+	current = a;
+	i = 0;
+	while (current)
+	{
+		if (!array_contains(lis, lis_size, current->number))
+		{
+			unsorted[i] = current->number;
+			i++;
+		}
+		current = current->next;
+	}
+	bubble_sort(unsorted, unsorted_size);
+	median = unsorted[unsorted_size / 2];
+	free(unsorted);
+	return (median);
 }
 
-static void bubble_sort(int *array, int size)
+static void	rotate_until_next_unsorted(t_list **a, int *lis, int lis_size)
 {
-    int i;
-    int j;
+	int	*numbers;
+	int	stack_size;
+	int	i;
+	int	j;
 
-    i = 0;
-    while (i < size - 1)
-    {
-        j = 0;
-        while (j < size - i - 1)
-        {
-            if (array[j] > array[j + 1])
-                ft_swap(&array[j], &array[j + 1]);
-            j++;
-        }
-        i++;
-    }
-}
-
-static int calculate_median(t_list *a, int unsorted_size, int *lis, int lis_size)
-{
-    t_list *current;
-    int *unsorted;
-    int median;
-    int i;
-
-    unsorted = malloc(unsorted_size * sizeof(int));
-    current = a;
-    i = 0;
-    while (current)
-    {
-        if (!array_contains(lis, lis_size, current->number))
-        {
-            unsorted[i] = current->number;
-            i++;
-        }
-        current = current->next;
-    }
-    bubble_sort(unsorted, unsorted_size);
-    median = unsorted[unsorted_size / 2];
-    free(unsorted);
-    return (median);
+	stack_size = list_size(*a);
+	list2array(*a, &numbers, stack_size);
+	i = 0;
+	while (array_contains(lis, lis_size, numbers[i]))
+		i++;
+	j = 0;
+	while (array_contains(lis, lis_size, numbers[stack_size - j - 1]))
+		j++;
+	if (i <= j)
+	{
+		while (i--)
+		{
+			rotate_stack_up(a);
+			ft_printf("ra\n");
+		}
+	}
+	else
+	{
+		while (j--)
+		{
+			rotate_stack_down(a);
+			ft_printf("rra\n");
+		}
+	}
+	free(numbers);
 }
 
 void	push_unsorted(t_list **a, t_list **b, int *lis, int lis_size)
 {
 	int	unsorted_size;
-    int median;
+	int	median;
 	int	i;
 
 	unsorted_size = list_size(*a) - lis_size;
-    median = calculate_median(*a, unsorted_size, lis, lis_size);
+	median = calculate_median(*a, unsorted_size, lis, lis_size);
 	i = 0;
 	while (i < unsorted_size)
 	{
-		while (array_contains(lis, lis_size, (*a)->number))
+		rotate_until_next_unsorted(a, lis, lis_size);
+		if ((*a)->number <= median)
 		{
-			rotate_stack_up(a);
-			ft_printf("ra\n");
+			push_top_stack(b, a);
+			ft_printf("pb\n");
 		}
-        if ((*a)->number <= median)
-        {
-            push_top_stack(b, a);
-            ft_printf("pb\n");
-        }
-        else
-        {
-            push_top_stack(b, a);
-		    ft_printf("pb\n");
-            rotate_stack_up(b);
+		else
+		{
+			push_top_stack(b, a);
+			ft_printf("pb\n");
+			rotate_stack_up(b);
 			ft_printf("rb\n");
-        }
+		}
 		i++;
 	}
 }
